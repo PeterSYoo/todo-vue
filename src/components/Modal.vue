c586f6571a2ce6246cca21f5d60df4c36efddbd7
Peter Yoo <trustintofu@gmail.com>
2023-04-25 23:31:44 -0700

    feat(Modal): Emits click event close on modal click.

---
+++ src/components/Modal.vue
@@ -0,0 +1,13 @@
+// --------------------------------- SCRIPT -------------------------------- ***
+<script setup lang="ts"></script>
+// ------------------------------------------------------------------------- ***
+// -------------------------------- TEMPLATE ------------------------------- ***
+<template>
+  <section
+    class="h-screen w-screen bg-black/20 flex justify-center items-center absolute top-0 left-0 cursor-pointer"
+    @click="$emit('close')"
+  >
+    Modal
+  </section>
+</template>
+// ------------------------------------------------------------------------- ***
