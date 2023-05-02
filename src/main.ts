03d7c4b6d47e2e289648191bfd48dffee14bf4eb
Peter Yoo <trustintofu@gmail.com>
2023-05-01 17:04:14 -0700

    feat(main): Calls the setupFirebase.

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,8 @@ import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 import router from './router';
+import { setup as setupFirebase } from './services/firebase.service';
+
+setupFirebase();
 
 createApp(App).use(router).mount('#app');
