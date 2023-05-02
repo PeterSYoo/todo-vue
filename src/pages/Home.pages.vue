a847f6dab0b1bee6aecc1b59c4a8727132144f17
Peter Yoo <trustintofu@gmail.com>
2023-05-01 17:05:35 -0700

    feat(Home): Uses auth properties from auth composoable.

---
+++ src/pages/Home.pages.vue
@@ -0,0 +1,37 @@
+// --------------------------------- SCRIPT -------------------------------- ***
+<script setup lang="ts">
+import { useAuth } from '../composables/auth/auth.composable';
+
+const auth = useAuth();
+
+const handleLoginWithGoogleClick = () => {
+  auth.loginInWithGoogle();
+};
+</script>
+// ------------------------------------------------------------------------- ***
+// -------------------------------- TEMPLATE ------------------------------- ***
+<template>
+  <main
+    class="justify-center flex flex-col items-center h-screen w-screen px-10"
+  >
+    <button
+      @click="handleLoginWithGoogleClick"
+      class="mt-2 border-b-2 border-black px-4 py-1 hover:text-black/50"
+    >
+      Google Login
+    </button>
+    <div class="border-b border-b-black w-32 mt-0.5"></div>
+    <ul class="pt-2 max-w-[1024px]">
+      <li>hasFailed: {{ auth.hasFailed }}</li>
+      <li>isLoading: {{ auth.isLoading }}</li>
+      <li>error: {{ auth.error }}</li>
+      <li>
+        user:
+        <div class="flex flex-col">
+          {{ auth.user.value }}
+        </div>
+      </li>
+    </ul>
+  </main>
+</template>
+// ------------------------------------------------------------------------- ***
